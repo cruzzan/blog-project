e0892a8dd958335f5c39da4fe49cc93cd673c02c
Sebastian Hultstrand <sebastian@storm-net.se>
2022-09-16 15:03:12 +0200

    Upgrades php to work with lumens requirements

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM nginx:1.22.0-alpine
 
-RUN apk add --update --no-cache php8-fpm php-mbstring
+RUN apk add --update --no-cache php81-fpm php81-mbstring
 
 COPY docker/php/entrypoint.sh /docker-entrypoint.d/php-entrypoint.sh
 COPY docker/nginx/site.conf /etc/nginx/conf.d/default.conf
-COPY public/test.php /usr/share/nginx/blog-project/index.php
 
 STOPSIGNAL SIGQUIT
--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -13,16 +13,16 @@ PHP_DISPLAY_STARTUP_ERRORS="On"
 PHP_ERROR_REPORTING="E_COMPILE_ERROR\|E_RECOVERABLE_ERROR\|E_ERROR\|E_CORE_ERROR"
 PHP_CGI_FIX_PATHINFO=0
 
-sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php8/php-fpm.d/www.conf
-sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php81/php-fpm.d/www.conf
+sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php81/php-fpm.d/www.conf
 
-sed -i "s|display_errors\s*=\s*Off|display_errors = ${PHP_DISPLAY_ERRORS}|i" /etc/php8/php.ini
-sed -i "s|display_startup_errors\s*=\s*Off|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|i" /etc/php8/php.ini
-#sed -i "s|error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = ${PHP_ERROR_REPORTING}|i" /etc/php8/php.ini
-sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php8/php.ini
-sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /etc/php8/php.ini
-sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php8/php.ini
-sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php8/php.ini
-sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php8/php.ini
+sed -i "s|display_errors\s*=\s*Off|display_errors = ${PHP_DISPLAY_ERRORS}|i" /etc/php81/php.ini
+sed -i "s|display_startup_errors\s*=\s*Off|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|i" /etc/php81/php.ini
+#sed -i "s|error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = ${PHP_ERROR_REPORTING}|i" /etc/php81/php.ini
+sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php81/php.ini
+sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /etc/php81/php.ini
+sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php81/php.ini
+sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php81/php.ini
+sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php81/php.ini
 
-exec "php-fpm8"
+exec "php-fpm81"
