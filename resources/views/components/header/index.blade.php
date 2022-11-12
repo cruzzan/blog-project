59fc0a08e2c2388a7fc113ad7169980493fab88c
Sebastian Hultstrand <sebastian@storm-net.se>
2022-11-12 19:57:12 +0100

    Starts to work with login flow

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -1,9 +1,14 @@
-<header class="navbar py-3 mb-3 border-bottom shadow fixed-top bg-light">
+<header class="navbar py-3 mb-3 border-bottom shadow sticky-top bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="/"><h3>SSoB</h3></a>
-        <div class="align-self-end">
-            <button type="button" class="btn btn-outline-success">Logga in</button>
-            <button type="button" class="btn btn-outline-primary">Skapa konto</button>
-        </div>
+
+        @if (Auth::check())
+            <p>You are logged in</p>
+        @else
+            <div>
+                <button type="button" class="btn btn-outline-success">Logga in</button>
+                <button type="button" class="btn btn-outline-primary">Skapa konto</button>
+            </div>
+        @endif
     </div>
 </header>
