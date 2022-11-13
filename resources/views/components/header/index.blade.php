531cec105a2082a6541f53261969401627763a86
Sebastian Hultstrand <sebastian@storm-net.se>
2022-11-13 12:00:08 +0100

    Adds navigation actions to header buttons

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -1,15 +1,15 @@
 <header class="navbar py-3 mb-3 border-bottom shadow sticky-top bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/"><h3>SSoB</h3></a>
+        <a class="navbar-brand" href="{{ route('home') }}"><h3>SSoB</h3></a>
 
         @if (Auth::check())
-            <p>You are logged in</p>
+            <a href="{{ route('logout') }}"><button class="btn btn-outline-danger">Log out</button></a>
         @endif
 
         @if(substr_count(URL::current(), 'login') <= 0 && !Auth::check())
             <div>
-                <button type="button" class="btn btn-outline-success">Logga in</button>
-                <button type="button" class="btn btn-outline-primary">Skapa konto</button>
+                <a href="{{ route('login') }}"><button type="button" class="btn btn-outline-success">Logga in</button></a>
+                <a href="#register"><button type="button" class="btn btn-outline-primary">Skapa konto</button></a>
             </div>
         @endif
     </div>
