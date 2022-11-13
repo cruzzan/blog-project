3a1f803cc0261ae81209964b37f409f286533d3c
Sebastian Hultstrand <sebastian@storm-net.se>
2022-11-13 11:48:26 +0100

    Styles the login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,21 +3,21 @@
 @section('content')
     <main class="form-signin m-auto">
         <div class="col-lg-4 mx-auto rounded">
-            <h2>Sign in</h2>
+            <h2 class="text-center">Sign in</h2>
             @error('email')
             <div class="alert alert-danger">{{ $message  }}</div>
             @enderror
-            <form method="POST" href="/login">
+            <form method="POST" href="/login" class="d-flex flex-column align-items-center">
                 @csrf
                 <div class="form-floating">
                     <input type="email" name="email" id="email" class="form-control" placeholder="Email">
                     <label for="email">Email:</label>
                 </div>
                 <div class="form-floating">
-                    <label for="password">Password:</label>
                     <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                    <label for="password">Password:</label>
                 </div>
-                <button type="submit" class="btn btn-primary w-75 align-self-center">Go...</button>
+                <button type="submit" class="btn btn-primary w-75">Go...</button>
             </form>
         </div>
     </main>
