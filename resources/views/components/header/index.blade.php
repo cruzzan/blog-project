ee4a4140003e81e137dba3e9a1be23f0f56da9bf
Sebastian Hultstrand <sebastian@storm-net.se>
2022-11-20 22:51:34 +0100

    Adds logo to the project

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -1,6 +1,8 @@
-<header class="navbar py-3 mb-3 border-bottom shadow sticky-top bg-light">
+<header class="navbar py-0 mb-3 border-bottom shadow sticky-top bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}"><h3>SSoB</h3></a>
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img class="logo" src="{{ Vite::asset('resources/static/images/ssob_logo.svg') }}" alt="SSoB">
+        </a>
 
         @if (Auth::check())
             <a href="{{ route('logout') }}"><button class="btn btn-outline-danger">Log out</button></a>
