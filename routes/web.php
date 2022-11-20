e7ea19bca0db5d291deb623bc9940cf4498e3fd5
Sebastian Hultstrand <sebastian@storm-net.se>
2022-11-20 21:11:12 +0100

    Adds a group around the authenticated routes

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,20 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.submit');
-
-Route::get('/{user_slug}/home', function () {
-    return view('');
-})->middleware('auth');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::resource('post', PostController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/{user_slug}/home', function () {
+        return view('');
+    })->middleware('auth');
 
+    Route::resource('post', PostController::class);
+});
