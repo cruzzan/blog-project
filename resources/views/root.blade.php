afaa8f6ec3eab8ad01e519116f10b031ed3e2351
Sebastian Hultstrand <sebastian@storm-net.se>
2022-12-01 23:08:48 +0100

    Removes redundant elements in root template

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -11,11 +11,7 @@
     <body>
         <x-header></x-header>
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    @yield('content')
-                </div>
-            </div>
+            @yield('content')
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         @vite(['resources/js/app.js'])
