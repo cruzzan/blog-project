42ad2a4c061a4bdd8155214ef84ce9dbad484a0e
Sebastian Hultstrand <sebastian@storm-net.se>
2022-12-01 21:12:18 +0100

    Adds feather icons to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
+import feather from 'feather-icons'
 
 import.meta.glob([
     '../static/images/**',
 ]);
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    feather.replace()
+})
