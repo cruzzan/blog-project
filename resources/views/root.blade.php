77721b03b9a32017977e64caa5f1c4b11be20902
Sebastian Hultstrand <sebastian@storm-net.se>
2022-12-13 21:00:48 +0100

    Updates to not use CDNs anymore

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,15 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Some sort of blog</title>
         <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/static/images/ssob_logo.svg') }}">
-        @vite(['resources/css/app.css'])
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        @vite(['resources/css/app.scss'])
     </head>
     <body>
         <x-header></x-header>
         <div class="container">
             @yield('content')
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         @vite(['resources/js/app.js'])
     </body>
 </html>
